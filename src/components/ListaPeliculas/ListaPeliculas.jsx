b510c230b967459188b098e58751f408873f10f0
josetrivino89 <josetrivino89@gmail.com>
2022-11-05 20:56:41 -0300

    se trabajo sobre las listas y las API

--- src/components/ListaPeliculas/ListaPeliculas.jsx
+++ src/components/ListaPeliculas/ListaPeliculas.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Card, ListGroup } from "react-bootstrap";
+import { Badge, Image, Card, ListGroup } from "react-bootstrap";
 import Loading from "../Layout/Loading";
 
+//*min 2.09.34 31-12-2022
 const ListaPeliculas = ({ titulo, movies }) => {
   const peliculas = movies.result.results; //* consultar porque trae esto de nuevo
 
@@ -13,8 +14,16 @@ const ListaPeliculas = ({ titulo, movies }) => {
           <Card.Title>{titulo}</Card.Title>
           <ListGroup>
             {peliculas.map((pelicula) => (
-              <ListGroup.Item key={pelicula.id}>
-                {pelicula.original_title}
+              <ListGroup.Item action key={pelicula.id}>
+                <div className="ms-2 me-auto d-flex justify-content-between">
+                  <Image roundedCircle src={`https://image.tmdb.org/t/p/w500/${pelicula.backdrop_path}`} fluid style={{height:`55px`, width:`55px`}}/>       
+                  <div className="fw-bold"><h5>{pelicula.original_title}</h5></div>
+                  
+                </div>
+                <Badge bg="danger" pill className="ml-2 me-auto">
+                  {pelicula.vote_average}
+                </Badge>
+               
               </ListGroup.Item>
             ))}
           </ListGroup>
